0e2e1e84f190991455170241786048a375204763
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2025-04-27 15:37:02 -0400

    ..

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 const faqItems = document.querySelectorAll('.faq-item');
 
+faqItems.forEach((item) => {
+    const question = item.querySelector('.question');
+    const icon = question.querySelector('img');
+    const answer = item.querySelector('.answer');
+
+    question.addEventListener('click', ()=>{
+        item.classList.toggle('active');
+
+        if (item.classList.contains('active')) {
+            icon.src = 'assets/images/icon-minus.svg';
+            answer.style.display = 'block';
+        } else {
+            icon.src = 'assets/images/icon-plus.svg';
+            answer.style.display = 'none';
+        }
+    });
+});
